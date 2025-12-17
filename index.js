9aad4651c7671ffa024591d59b71f8ab29375394
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-12-17 18:43:16 +0200

    feat(search page): Add the input for search match

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// For search part of the matches
+function searchDropdown() {
+    return {
+        open: false,
+        selectedValue: '',
+        items: [
+            'مسابقات فوتبال دستی طهران کاپ1403',
+            'مسابقات اسنوکر طهران کاپ1401',
+            'مسابقات اسنوکر  آقایان طهران کاپ1403',
+            'مسابقات اسنوکر  بانوان طهران کاپ1403',
+        ],
+        
+        get filteredItems() {
+            return this.items;
+        },
+        
+        selectItem(item) {
+            this.selectedValue = item;
+            this.open = false;
+        }
+    }
+}
